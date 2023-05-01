86cb8d07b73b6b7c13348fc4e77de755a6180eaf
Lucas Lengrand <lucaslengranddev@gmail.com>
2023-05-02 00:05:59 +0200

    protection .env.local

--- src/components/Administration.js
+++ src/components/Administration.js
@@ -39,7 +39,7 @@ const Administration = () => {
         authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
         databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
         projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: "optiquechatenay-44520.appspot.com",
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.REACT_APP_FIREBASE_APP_ID,
         measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
