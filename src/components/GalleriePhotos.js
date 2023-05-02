c603e56baeb13999d8daf26222f5a493694fe4ae
Lucas Lengrand <lucaslengranddev@gmail.com>
2023-05-02 15:02:59 +0200

    mise à jour mise en page

--- src/components/GalleriePhotos.js
+++ src/components/GalleriePhotos.js
@@ -3,10 +3,7 @@ import React from 'react'
 function GalleriePhotos() {
   return (
     <section id="decouvrez" className='sectionGalleriePhotos'>
-        <div className='titreGalleriePhotos'>
-            <h2>Découvrez notre magasin</h2>
-            <h3>Cliquez sur une image et découvrez notre galerie photo</h3>
-        </div>
+
         <div className='imagesGalleriesContainer'>
         <div className='imagesHorizontalContainer'>
         <a href="/gallerie" alt="Galleries"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesHorizon%2FIMG-20221116-WA0001.jpg?alt=media&token=559ac80c-7394-421e-8847-f73b7ccd8b05" alt="magasin"></img></a>
@@ -15,6 +12,9 @@ function GalleriePhotos() {
         <div className='imagesVerticales'>
         <a href="/gallerie" alt="Galleries"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesVertical%2F5bd0d265-3e52-41ce-919f-ee2b72194ea9.jpg?alt=media&token=86067f1c-0115-4331-b392-0e477b379760" alt="magasin" /></a>
         </div>
+        </div>
+        <div className='titreGalleriePhotos'>
+            <h4>Cliquez sur une image et découvrez notre galerie photo</h4>
         </div>
     </section>
   )
