217e5c4bd09ab3dc3ed52edcff7d0af42eef2133
Lucas Lengrand <lucaslengranddev@gmail.com>
2023-05-05 19:28:25 +0200

    responsive gallerie photo

--- src/components/GalleriePhotos.js
+++ src/components/GalleriePhotos.js
@@ -8,17 +8,17 @@ function GalleriePhotos() {
         <div className='imagesGalleriesContainer'>
         <div className='imagesHorizontalContainer'>
         <div className='premiereImageGallerie'>
-        <a href="/gallerie" alt="Galleries"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesHorizon%2FIMG-20221116-WA0001.jpg?alt=media&token=559ac80c-7394-421e-8847-f73b7ccd8b05" alt="magasin"></img></a>
+        <a href="/gallerie" alt="Galleries"title="Gallerie de photos"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesHorizon%2FIMG-20221116-WA0001.jpg?alt=media&token=559ac80c-7394-421e-8847-f73b7ccd8b05" alt="magasin"></img></a>
         </div>
         <div className='secondeImageGallerie'>
-        <a href="/gallerie" alt="Galleries"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesHorizon%2F2021-10-15.jpg?alt=media&token=d058de1e-770e-44c4-9d4e-7f2ad0e3d62b" alt="magasin"></img></a>
+        <a href="/gallerie" alt="Galleries"title="Gallerie de photos"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesHorizon%2F2021-10-15.jpg?alt=media&token=d058de1e-770e-44c4-9d4e-7f2ad0e3d62b" alt="magasin"></img></a>
         </div>
         <div className='troisiemeImageGallerie'>
-        <a href="/gallerie" alt="Galleries"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesHorizon%2FIMG-20221116-WA0004.jpg?alt=media&token=5ec28827-fd6d-4f01-b557-006140e5143a" alt="magasin"></img></a>
+        <a href="/gallerie" alt="Galleries"title="Gallerie de photos"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesHorizon%2FIMG-20221116-WA0004.jpg?alt=media&token=5ec28827-fd6d-4f01-b557-006140e5143a" alt="magasin"></img></a>
         </div>
         </div>
         <div className='imagesVerticales'>
-        <a href="/gallerie" alt="Galleries"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesVertical%2F5bd0d265-3e52-41ce-919f-ee2b72194ea9.jpg?alt=media&token=86067f1c-0115-4331-b392-0e477b379760" alt="magasin" /></a>
+        <a href="/gallerie" alt="Galleries" title="Gallerie de photos"><img src="https://firebasestorage.googleapis.com/v0/b/optiquechatenay-44520.appspot.com/o/ImagesVertical%2F5bd0d265-3e52-41ce-919f-ee2b72194ea9.jpg?alt=media&token=86067f1c-0115-4331-b392-0e477b379760" alt="magasin" /></a>
         </div>
         </div>
         <div className='titreGalleriePhotos'>
