a249770198c91a51961cdbaf51e1cc3588efa3fd
Lucas Lengrand <lucaslengranddev@gmail.com>
2023-12-10 02:29:11 +0100

    Changement bloc admin gallerie photo et bloc horaires

--- src/components/StoreHours.js
+++ src/components/StoreHours.js
@@ -1,138 +1,108 @@
 import React, { useState, useEffect } from 'react';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 const StoreHours = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
+  const [storeHours, setStoreHours] = useState([]);
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  const placeId = process.env.REACT_APP_GOOGLE_PLACE_ID;
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentDate(new Date());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    const fetchPlaceDetails = async () => {
+      try {
+        const proxyUrl = 'https://api.allorigins.win/raw?url=';
+        const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`;
+        const response = await fetch(`${proxyUrl}${encodeURIComponent(url)}`);
+        const data = await response.json();
 
-  const joursSemaine = [
-    "Dimanche",
-    "Lundi",
-    "Mardi",
-    "Mercredi",
-    "Jeudi",
-    "Vendredi",
-    "Samedi",
-  ];
+        if (response.ok) {
+          const periods = data.result?.opening_hours?.periods || [];
+          setStoreHours(periods);
+        } else {
+          console.error('Error fetching place details:', data.error_message || 'Unknown error');
+        }
+      } catch (error) {
+        console.error('Error fetching place details:', error.message);
+      }
+    };
 
-  const horairesMagasin = {
-    Lundi: { matin: { ouverture: "09:30", fermeture: "13:00" }, apresMidi: { ouverture: "14:30", fermeture: "19:00" } },
-    Mardi: { matin: { ouverture: "09:30", fermeture: "13:00" }, apresMidi: { ouverture: "14:30", fermeture: "19:00" } },
-    Mercredi: { matin: { ouverture: "9:30", fermeture: "13:00" }, apresMidi: { ouverture: "14:30", fermeture: "19:30" } },
-    Jeudi: { matin: { ouverture: "09:30", fermeture: "13:00" }, apresMidi: { ouverture: "14:30", fermeture: "19:00" } },
-    Vendredi: { matin: { ouverture: "09:30", fermeture: "13:00" }, apresMidi: { ouverture: "14:30", fermeture: "19:00" } },
-    Samedi: { matin: { ouverture: "09:30", fermeture: "13:00" }, apresMidi: { ouverture: "14:30", fermeture: "18:00" } },
-    Dimanche: { matin: { ouverture: "Fermé", fermeture: "Fermé" }, apresMidi: { ouverture: "Fermé", fermeture: "Fermé" } }
+    fetchPlaceDetails();
+
+    return () => {
+      // Nettoyer les effets, annuler des requêtes en cours, etc.
+    };
+  }, [apiKey, placeId]);
+
+  const formatTime = (time) => {
+    if (time === null || time === undefined) {
+      console.log('Invalid time:', time);
+      return 'N/A';
+    }
+
+    return `${time.slice(0, 2)}:${time.slice(2)}`;
   };
 
-  const jourActuel = joursSemaine[currentDate.getDay()];
-  const horairesOuverture = horairesMagasin[jourActuel];
+  const getNextOpeningDay = () => {
+    const today = new Date().getDay();
+    const nextOpeningDayIndex = storeHours.findIndex(period => period.open?.day === (today + 1) % 7);
+    return nextOpeningDayIndex !== -1 ? getDayName((today + 1) % 7) : null;
+  };
+  
+  const getDayName = (dayIndex) => {
+    const daysOfWeek = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+    return daysOfWeek[dayIndex];
+  };
 
-  const isMagasinOuvert = () => {
-    const { matin, apresMidi } = horairesOuverture;
-    const heureOuvertureMatin = parseInt(matin.ouverture.split(":")[0]);
-    const minuteOuvertureMatin = parseInt(matin.ouverture.split(":")[1]);
-    const heureFermetureMatin = parseInt(matin.fermeture.split(":")[0]);
-    const minuteFermetureMatin = parseInt(matin.fermeture.split(":")[1]);
-    const heureOuvertureApresMidi = parseInt(apresMidi.ouverture.split(":")[0]);
-    const minuteOuvertureApresMidi = parseInt(apresMidi.ouverture.split(":")[1]);
-    const heureFermetureApresMidi = parseInt(apresMidi.fermeture.split(":")[0]);
-    const minuteFermetureApresMidi = parseInt(apresMidi.fermeture.split(":")[1]);
-
-    const heureActuelle = currentDate.getHours();
-    const minuteActuelle = currentDate.getMinutes();
-
-    const heureActuelleMinutes = heureActuelle * 60 + minuteActuelle;
-    const heureOuvertureMatinMinutes =
-      heureOuvertureMatin * 60 + minuteOuvertureMatin;
-    const heureFermetureMatinMinutes =
-      heureFermetureMatin * 60 + minuteFermetureMatin;
-    const heureOuvertureApresMidiMinutes =
-      heureOuvertureApresMidi * 60 + minuteOuvertureApresMidi;
-    const heureFermetureApresMidiMinutes =
-      heureFermetureApresMidi * 60 + minuteFermetureApresMidi;
-
-    const magasinOuvertMatin =
-      heureActuelleMinutes >= heureOuvertureMatinMinutes &&
-      heureActuelleMinutes <= heureFermetureMatinMinutes;
-    const magasinOuvertApresMidi =
-      heureActuelleMinutes >= heureOuvertureApresMidiMinutes &&
-      heureActuelleMinutes <= heureFermetureApresMidiMinutes;
-
-      
-      const getNextOpeningHour = () => {
-        const { matin, apresMidi } = horairesOuverture;
-    
-        const heureActuelle = currentDate.getHours();
-        // eslint-disable-next-line no-unused-vars
-        const minuteActuelle = currentDate.getMinutes();
-    
-        // Si le magasin est déjà ouvert
-        if (heureActuelle >= parseInt(matin.ouverture.split(':')[0]) && heureActuelle < parseInt(apresMidi.fermeture.split(':')[0])) {
-          return `Le magasin est ouvert jusqu'à ${apresMidi.fermeture}`;
-        }
-    
-        // Si le magasin ouvre dans la journée actuelle
-        if (heureActuelle < parseInt(matin.ouverture.split(':')[0])) {
-          return `Le magasin est fermé, il ouvre à ${matin.ouverture}`;
-        } else if (heureActuelle < parseInt(apresMidi.ouverture.split(':')[0])) {
-          return `Le magasin est fermé, il ouvre à ${apresMidi.ouverture}`;
-        }
-    
-        // Si le magasin est fermé et qu'il n'ouvre pas dans la journée actuelle
-        const joursOuverture = Object.keys(horairesMagasin);
-        const indexJourActuel = joursOuverture.findIndex(jour => jour === jourActuel);
-let i = 1;
-while (i < joursOuverture.length) {
-  const jourSuivant = joursOuverture[(indexJourActuel + i) % joursOuverture.length];
-  const horairesSuivant = horairesMagasin[jourSuivant];
-  if (horairesSuivant.matin.ouverture !== "Fermé") {
-    return `Le magasin est fermé il ouvre ${jourSuivant} à ${horairesSuivant.matin.ouverture}`;
-  } else if (horairesSuivant.apresMidi.ouverture !== "Fermé") {
-    return `Le magasin est fermé il ouvre ${jourSuivant} à ${horairesSuivant.apresMidi.ouverture}`;
-  }
-  i++;
-}
-
-// Si le magasin n'a plus de jour d'ouverture dans la semaine
-return `Le magasin est fermé pour la semaine`};
-
-    if (magasinOuvertMatin && magasinOuvertApresMidi) {
-      return (
-        <div>
-           Ouvert maintenant !<br/> Horaires : {matin.ouverture} -{" "}
-          {matin.fermeture} / {apresMidi.ouverture} - {apresMidi.fermeture}
-        </div>
-      );
-    } else if (magasinOuvertMatin) {
-      return (
-        <div>
-           Ouvert maintenant !<br/> Horaires : {matin.ouverture} -{" "}
-          {matin.fermeture}
-        </div>
-      );
-    } else if (magasinOuvertApresMidi) {
-      return (
-        <div>
-         <span> <AccessTimeIcon /> </span> Ouvert maintenant !<br/> Horaires : {apresMidi.ouverture} -{" "}
-          {apresMidi.fermeture}
-        </div>
-      );
-    } else {
-      return <div>{getNextOpeningHour()}</div>;
+  const getNextOpeningHours = () => {
+    const nextOpeningDay = getNextOpeningDay();
+    const openingHours = storeHours.find(period => period.open?.day === (new Date().getDay() + 1) % 7);
+  
+    console.log('nextOpeningDay:', nextOpeningDay);
+    console.log('openingHours:', openingHours);
+  
+    if (openingHours) {
+      const { open, close } = openingHours;
+      console.log('open:', open);
+      console.log('close:', close);
+      return `${formatTime(open?.time)} à ${formatTime(close?.time)}`;
     }
+  
+    return 'N/A';
+  };
+  
+
+  const isStoreOpen = () => {
+    const today = new Date().getDay();
+    const currentHour = new Date().getHours();
+    const currentMinute = new Date().getMinutes();
+    const todayOpeningHours = storeHours.find(period => period.open?.day === today);
+
+    return todayOpeningHours
+      ? currentHour * 60 + currentMinute >= parseInt(todayOpeningHours.open?.time)
+        && currentHour * 60 + currentMinute <= parseInt(todayOpeningHours.close?.time)
+      : false;
   };
 
   return (
-    <div className='blocTextHeures'>
-      {isMagasinOuvert()}
+    <div>
+      <h2>Horaires du magasin</h2>
+      {storeHours.length === 0 ? (
+        <p>Horaires non disponibles</p>
+      ) : (
+        <div>
+          {isStoreOpen() ? (
+            <p>Le magasin est ouvert aujourd'hui.</p>
+          ) : (
+            <div>
+              <p>Actuellement, le magasin est fermé.</p>
+              <p>
+                Prochaines horaires d'ouverture :
+                {getNextOpeningDay() !== null ? `${getNextOpeningDay()} de ${getNextOpeningHours()}` : 'N/A'}
+              </p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
-  )};
+  );
+};
 
-  export default StoreHours;
+export default StoreHours;
