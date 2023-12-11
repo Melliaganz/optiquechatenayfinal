38cfe2ac4a3b702bf07a7f9030537ac17651305d
Lucas Lengrand <lucaslengranddev@gmail.com>
2023-12-11 23:27:49 +0100

    correction bloc horaires

--- src/components/Horaires.js
+++ src/components/Horaires.js
@@ -1,61 +1,109 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import StoreHours from './StoreHours';
 
-function Horaires(props) {
+const Horaires = () => {
+  const [apiStoreHours, setApiStoreHours] = useState([]);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const popupRef = useRef(null);
+
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY; // Remplacez par votre clé API Google Places
+  const placeId = process.env.REACT_APP_GOOGLE_PLACE_ID; // Remplacez par votre ID de lieu
+
+  const getDayName = (dayIndex) => {
+    const daysTranslations = [
+      'Dimanche',
+      'Lundi',
+      'Mardi',
+      'Mercredi',
+      'Jeudi',
+      'Vendredi',
+      'Samedi',
+    ];
+    return daysTranslations[dayIndex] || '';
+  };
+
+  const formatTime = (time) => {
+    if (time === null || time === undefined) {
+      return 'N/A';
+    }
+    return `${time.slice(0, 2)}:${time.slice(2)}`;
+  };
 
   useEffect(() => {
-    function handleScroll() {
-      if (popupRef.current) {
-        setIsPopupOpen(false);
+    const fetchApiStoreHours = async () => {
+      try {
+        const proxyUrl = 'https://api.allorigins.win/raw?url=';
+        const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`;
+        const response = await fetch(`${proxyUrl}${encodeURIComponent(url)}`);
+        const data = await response.json();
+
+        if (response.ok) {
+          const openingHours = data.result?.opening_hours;
+          setApiStoreHours(openingHours?.periods || []);
+        } else {
+          console.error('Erreur lors de la récupération des détails du lieu :', data.error_message || 'Erreur inconnue');
+        }
+      } catch (error) {
+        console.error('Erreur lors de la récupération des détails du lieu :', error.message);
       }
+    };
+
+    fetchApiStoreHours();
+  }, [apiKey, placeId]);
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  const formatDayHours = (periods) => {
+    if (!periods) {
+      return 'N/A';
     }
 
-    window.addEventListener("scroll", handleScroll);
+    const formattedPeriods = periods.reduce((acc, period) => {
+      const dayName = getDayName(period.open.day);
+      const formattedStartHour = formatTime(period.open.time);
+      const formattedEndHour = formatTime(period.close.time);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  function togglePopup() {
-    setIsPopupOpen(!isPopupOpen);
-  }
+      const formattedPeriod = `${dayName}: ${formattedStartHour} - ${formattedEndHour}`;
+      const index = acc.findIndex((item) => item.startsWith(dayName));
+      if (index !== -1) {
+        // Si le jour est déjà présent, ajoute le créneau horaire à celui existant
+        const existingDay = acc[index];
+        const combinedPeriod = `${existingDay.slice(existingDay.indexOf(':') + 2)} / ${formattedPeriod.slice(formattedPeriod.indexOf(':') + 2)}`;
+        acc[index] = `${dayName}: ${combinedPeriod}`;
+      } else {
+        acc.push(formattedPeriod);
+      }
+
+      return acc;
+    }, []);
+
+    return formattedPeriods.join('\n');
+  };
 
   return (
     <div className='blocHoraires'>
-                    <div className='blocHorairesTexte'>
-                      <div className='encadréOuvertFermé'>
-                        <StoreHours/>
-                        </div>
-                        <button onClick={togglePopup} className='bouttonHoraires'>
-                            Voir les horaires
-                        </button>
-                        {isPopupOpen && (
-                          <div 
-                          ref={popupRef}
-                          className='tableauHoraires'
-                          style={{ 
-                            position: "fixed", 
-                            width: "300px", height: "400px", padding: "40px" }}>
-                              <div className='textHorairesPopup'>
-                          <h2>Horaires d'Optique Chatenay</h2>
-                          <ul>
-                            <li>Lundi : Fermé / 14:30-19:00</li>
-                            <li>Mardi : 09:30-13:00 / 14:30-19:00</li>
-                            <li>Mercredi : 09:30-13:00 / 14:30-19:0</li>
-                            <li>Jeudi : 09:30-13:00 / 14:30-19:00</li>
-                            <li>Vendredi : 09:30-13:00 / 14:30-19:00</li>
-                            <li>Samedi : 09:30-13:00 / 14:30-18:00</li>
-                            <li>Dimanche : Fermé</li>
-                          </ul>
-                          </div>
-                          <button className='bouttonMenuHoraires' onClick={togglePopup}>Fermer</button>
-                        </div>
-                        )}
-                    </div>
-                </div>
-  )
-}
+      <div className='blocHorairesTexte'>
+        <div className='encadréOuvertFermé'>
+          <StoreHours storeHours={apiStoreHours} />
+        </div>
+        <button onClick={togglePopup} className='bouttonHoraires'>
+          Voir les horaires
+        </button>
+        {isPopupOpen && (
+          <div className='tableauHoraires'>
+            <div className='textHorairesPopup'>
+              <h2>Horaires du magasin</h2>
+              <pre>{formatDayHours(apiStoreHours)}</pre>
+            </div>
+            <button className='bouttonMenuHoraires' onClick={togglePopup}>
+              Fermer
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Horaires
+export default Horaires;
--- src/components/StoreHours.js
+++ src/components/StoreHours.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 const StoreHours = () => {
   const [storeHours, setStoreHours] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isOpenNow, setIsOpenNow] = useState(false);
   const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
   const placeId = process.env.REACT_APP_GOOGLE_PLACE_ID;
 
@@ -15,8 +16,9 @@ const StoreHours = () => {
         const data = await response.json();
 
         if (response.ok) {
-          const periods = data.result?.opening_hours?.periods || [];
-          setStoreHours(periods);
+          const openingHours = data.result?.opening_hours;
+          setStoreHours(openingHours?.periods || []);
+          setIsOpenNow(openingHours?.open_now || false);
           setLoading(false);
         } else {
           console.error('Error fetching place details:', data.error_message || 'Unknown error');
@@ -31,7 +33,6 @@ const StoreHours = () => {
     fetchPlaceDetails();
 
     return () => {
-      // Nettoyer les effets, annuler des requêtes en cours, etc.
     };
   }, [apiKey, placeId]);
 
@@ -43,31 +44,8 @@ const StoreHours = () => {
     return `${time.slice(0, 2)}:${time.slice(2)}`;
   };
 
-  const getNextOpeningDay = () => {
-    const today = new Date().getDay();
-    const nextOpeningDayIndex = storeHours.findIndex(period => period.open?.day === (today + 1) % 7);
-    return nextOpeningDayIndex !== -1 ? getDayName((today + 1) % 7) : null;
-  };
-
-  const getDayName = (dayIndex) => {
-    const daysOfWeek = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-    return daysOfWeek[dayIndex];
-  };
-
-  const getNextOpeningHours = () => {
-    const openingHours = storeHours.find(period => period.open?.day === (new Date().getDay() + 1) % 7);
-
-    if (openingHours) {
-      const { open, close } = openingHours;
-      return `${formatTime(open?.time)} à ${formatTime(close?.time)}`;
-    }
-
-    return 'N/A';
-  };
-
   const getNextClosingHours = () => {
     const closingHours = storeHours.find(period => period.close?.day === (new Date().getDay() + 1) % 7);
-
     if (closingHours) {
       const { close } = closingHours;
       return `${formatTime(close?.time)}`;
@@ -76,16 +54,19 @@ const StoreHours = () => {
     return 'N/A';
   };
 
-  const isStoreOpen = () => {
-    const today = new Date().getDay();
-    const currentHour = new Date().getHours();
-    const currentMinute = new Date().getMinutes();
-    const todayOpeningHours = storeHours.find(period => period.open?.day === today);
+  const getNextOpeningDayAndHours = () => {
+    const nextOpeningDayIndex = storeHours.findIndex(period => period.open?.day > new Date().getDay());
+    if (nextOpeningDayIndex !== -1) {
+      const nextOpeningHours = storeHours[nextOpeningDayIndex].open;
+      return `${getDayName(nextOpeningHours?.day)} à ${formatTime(nextOpeningHours?.time)}`;
+    }
 
-    return todayOpeningHours
-      ? currentHour * 60 + currentMinute >= parseInt(todayOpeningHours.open?.time)
-        && currentHour * 60 + currentMinute <= parseInt(todayOpeningHours.close?.time)
-      : false;
+    return 'N/A';
+  };
+
+  const getDayName = (dayIndex) => {
+    const daysOfWeek = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+    return daysOfWeek[dayIndex];
   };
 
   return (
@@ -93,24 +74,18 @@ const StoreHours = () => {
       <h2>Horaires du magasin</h2>
       {loading ? (
         <p>Chargement des horaires...</p>
-      ) : storeHours.length === 0 ? (
-        <p>Horaires non disponibles</p>
+      ) : isOpenNow ? (
+        <div>
+          <p>Le magasin est ouvert aujourd'hui.</p>
+          <p>Il ferme à {getNextClosingHours()}</p>
+        </div>
       ) : (
         <div>
-          {isStoreOpen() ? (
-            <div>
-              <p>Le magasin est ouvert aujourd'hui.</p>
-              <p>Il ferme à {getNextClosingHours()}</p>
-            </div>
-          ) : (
-            <div>
-              <p>Actuellement, le magasin est fermé.</p>
-              <p>
-                Prochaine ouverture :&nbsp;
-                {getNextOpeningDay() !== null ? `${getNextOpeningDay()} de ${getNextOpeningHours()}` : 'N/A'}
-              </p>
-            </div>
-          )}
+          <p>Actuellement, le magasin est fermé.</p>
+          <p>
+            Prochaine ouverture :&nbsp;
+            {getNextOpeningDayAndHours()}
+          </p>
         </div>
       )}
     </div>
