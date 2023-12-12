b5c77c673c5fe4851eb60f381c8152e345a9199b
Lucas Lengrand <lucaslengranddev@gmail.com>
2023-12-12 23:14:33 +0100

    correction bloc horaire

--- src/components/StoreHours.js
+++ src/components/StoreHours.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const StoreHours = () => {
-  const [storeHours, setStoreHours] = useState([]);
+  const [storeDetails, setStoreDetails] = useState(null);
+  const [nextOpeningDay, setNextOpeningDay] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [isOpenNow, setIsOpenNow] = useState(false);
+  const [hoursUnavailable, setHoursUnavailable] = useState(false);
+
   const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
   const placeId = process.env.REACT_APP_GOOGLE_PLACE_ID;
 
@@ -14,80 +16,127 @@ const StoreHours = () => {
         const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`;
         const response = await fetch(`${proxyUrl}${encodeURIComponent(url)}`);
         const data = await response.json();
+        setStoreDetails(data.result);
 
-        if (response.ok) {
-          const openingHours = data.result?.opening_hours;
-          setStoreHours(openingHours?.periods || []);
-          setIsOpenNow(openingHours?.open_now || false);
-          setLoading(false);
+        // Extract opening periods for further processing
+        const openingPeriods = data.result?.opening_hours?.periods;
+        if (openingPeriods) {
+          const nextOpening = getNextOpeningTime(openingPeriods);
+          setNextOpeningDay(nextOpening?.open?.day);
         } else {
-          console.error('Error fetching place details:', data.error_message || 'Unknown error');
-          setLoading(false);
+          setHoursUnavailable(true);
         }
+
+        setLoading(false); // Set loading to false after data is fetched
       } catch (error) {
-        console.error('Error fetching place details:', error.message);
-        setLoading(false);
+        console.error('Error fetching place details:', error);
+        setLoading(false); // Set loading to false in case of an error
+        setHoursUnavailable(true);
       }
     };
 
     fetchPlaceDetails();
-
-    return () => {
-    };
   }, [apiKey, placeId]);
 
-  const formatTime = (time) => {
-    if (time === null || time === undefined) {
-      return 'N/A';
+  const renderStoreStatus = () => {
+    if (loading) {
+      return <p>Chargement des informations...</p>;
     }
 
-    return `${time.slice(0, 2)}:${time.slice(2)}`;
-  };
-
-  const getNextClosingHours = () => {
-    const closingHours = storeHours.find(period => period.close?.day === (new Date().getDay() + 1) % 7);
-    if (closingHours) {
-      const { close } = closingHours;
-      return `${formatTime(close?.time)}`;
+    if (hoursUnavailable) {
+      return <p>Les horaires ne sont pas disponibles pour le moment.</p>;
     }
 
-    return 'N/A';
-  };
-
-  const getNextOpeningDayAndHours = () => {
-    const nextOpeningDayIndex = storeHours.findIndex(period => period.open?.day > new Date().getDay());
-    if (nextOpeningDayIndex !== -1) {
-      const nextOpeningHours = storeHours[nextOpeningDayIndex].open;
-      return `${getDayName(nextOpeningHours?.day)} à ${formatTime(nextOpeningHours?.time)}`;
+    if (!storeDetails || storeDetails.hasOwnProperty('permanently_closed')) {
+      return <p>Informations sur le magasin non disponibles.</p>;
     }
 
-    return 'N/A';
+    const isOpenNow = storeDetails.opening_hours && storeDetails.opening_hours.open_now;
+
+    const statusMessage = isOpenNow ? "Le magasin est ouvert actuellement." : "Le magasin est fermé actuellement.";
+
+    if (isOpenNow) {
+      const nextClosing = getNextClosingTime(storeDetails.opening_hours.periods);
+      const closingTime = formatTime(nextClosing.close);
+
+      return (
+        <div className='blocTextHeures'>
+          <p>{statusMessage}</p>
+          <p>Le magasin est ouvert jusqu'à {closingTime}.</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className='blocTextHeures'>
+          <p>{statusMessage}</p>
+          {nextOpeningDay !== null && (
+            <p>Le magasin ouvrira le {getDayName(nextOpeningDay)}.</p>
+          )}
+          {nextOpeningDay !== null && renderNextOpeningHours(storeDetails.opening_hours.periods)}
+        </div>
+      );
+    }
   };
 
-  const getDayName = (dayIndex) => {
-    const daysOfWeek = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-    return daysOfWeek[dayIndex];
+  const getDayName = (dayNumber) => {
+    // Suppose that Sunday is day 0
+    const days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+    return days[dayNumber];
+  };
+
+  const renderNextOpeningHours = (periods) => {
+    const nextOpening = periods.find(period => period.open.day === nextOpeningDay);
+    if (nextOpening) {
+      const openingTime = formatTime(nextOpening.open);
+      const closingTime = formatTime(nextOpening.close);
+
+      return (
+        <p>Les prochains horaires d'ouverture sont de {openingTime} à {closingTime}.</p>
+      );
+    }
+    return null;
+  };
+
+  const getNextClosingTime = (periods) => {
+    const now = new Date();
+    const currentDay = now.getDay();
+    const currentHour = now.getHours() * 100 + now.getMinutes();
+
+    const nextClosing = periods.find(
+      (period) => period.open.day > currentDay || (period.open.day === currentDay && period.close.time > currentHour)
+    );
+
+    return nextClosing || periods[0];
+  };
+
+  const getNextOpeningTime = (periods) => {
+    const now = new Date();
+    const currentDay = now.getDay();
+    const currentHour = now.getHours() * 100 + now.getMinutes();
+
+    const nextOpening = periods.find(
+      (period) => period.open.day > currentDay || (period.open.day === currentDay && period.open.time > currentHour)
+    );
+
+    if (nextOpening) {
+      return nextOpening;
+    } else {
+      const nextDayOpening = periods.find((period) => period.open.day > currentDay);
+      return nextDayOpening || periods[0];
+    }
+  };
+
+  const formatTime = (dateTime) => {
+    const hours = Math.floor(dateTime.time / 100);
+    const minutes = dateTime.time % 100;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hours}:${formattedMinutes}`;
   };
 
   return (
-    <div>
-      <h2>Horaires du magasin</h2>
-      {loading ? (
-        <p>Chargement des horaires...</p>
-      ) : isOpenNow ? (
-        <div>
-          <p>Le magasin est ouvert aujourd'hui.</p>
-          <p>Il ferme à {getNextClosingHours()}</p>
-        </div>
-      ) : (
-        <div>
-          <p>Actuellement, le magasin est fermé.</p>
-          <p>
-            Prochaine ouverture :&nbsp;
-            {getNextOpeningDayAndHours()}
-          </p>
-        </div>
-      )}
+    <div className='blocTextHeures'>
+      <h2>Horaires du d'Optique Chatenay</h2>
+      {renderStoreStatus()}
     </div>
   );
 };
